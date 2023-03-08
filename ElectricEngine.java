9548a41903a1b7b07a2a0999e9069d2810e79986
Sandeep <sandeepk121234@gmail.com>
2023-03-08 23:02:07 +0530

    Stop method added to ElectricEngine class

---
+++ ElectricEngine.java
@@ -0,0 +1,10 @@
+public class ElectricEngine{
+	public void start() {
+		Sytstem.out.println("Electric Engine Started");
+		
+		
+	}
+	public void stop() {
+		Sytstem.out.println("Electric Engine Stoped");
+
+}
